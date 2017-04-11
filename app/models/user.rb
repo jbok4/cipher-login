8fab5e00df50b25ccdbe16fb07eddf4b178556a3
jbok4 <jbok4@aol.com>
2017-04-11 08:43:11 -0400

    controllers setup and tests passing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ApplicationRecord
+  has_many :doctors
+
+  validates :name, presence: true
+  validates :name, uniqueness: true
+
+  validates :email, presence: true
+  validates_format_of :email, with: /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/ 
+
+  validates :phone, presence: true
+  
+  validates :insurance_co, presence: true
+
+  validates :insurance_number, presence: true
+  validates :insurance_number, uniqueness: true
 end
